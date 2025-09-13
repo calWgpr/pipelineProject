b956005e73ec369113f1d019bc7337a4ba68c166
Tsiory Fahasoavana <166718289+calWgpr@users.noreply.github.com>
2025-09-13 14:09:44 +0300

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 public class App {
 
-    // Create a logger for this class
+    // Create a logger for this classes
     private static final Logger logger = Logger.getLogger(App.class.getName());
 
     public static void main(String[] args) {
