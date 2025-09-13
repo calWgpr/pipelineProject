c53bf24c55d58f4d722122b8d9cb363e214cd3e2
Tsiory Fahasoavana <166718289+calWgpr@users.noreply.github.com>
2025-09-13 15:09:59 +0300

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,10 +2,10 @@ package com.example;
 
 
 
-@SuppressWarnings("unused")
+
 public class App {
 
-    @SuppressWarnings("unused")
+  
     public static void main(String[] args) {
         // nothing happens
     }
