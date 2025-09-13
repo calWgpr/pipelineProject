839aab3a1fe551855ee885cc6fa180b17d750add
Tsiory Fahasoavana <166718289+calWgpr@users.noreply.github.com>
2025-09-13 11:17:26 +0300

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -2,10 +2,13 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 class AppTest {
 
     @Test
     void testMain() {
-        App.main(new String[]{});
+        // Assert that calling main() does not throw any exception
+        assertDoesNotThrow(() -> App.main(new String[]{}));
     }
 }
