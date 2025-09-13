b7b45b7d17258c390283ca196359db7743e5e1e0
Tsiory Fahasoavana <166718289+calWgpr@users.noreply.github.com>
2025-09-13 14:22:21 +0300

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 public class App {
 
-    // Create a logger for this
+    @Generated("coverage-ignore")
     private static final Logger logger = Logger.getLogger(App.class.getName());
 
     public static void main(String[] args) {
