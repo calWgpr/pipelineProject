0f216e0b367381a2dc02d07e54e353547b695cb0
Tsiory Fahasoavana <166718289+calWgpr@users.noreply.github.com>
2025-09-13 14:59:25 +0300

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,7 @@
 package com.example;
 
-import java.util.logging.Logger;
-
 public class App {
-
-    private static final Logger logger = Logger.getLogger(App.class.getName());
-
     public static void main(String[] args) {
-        logger.info("Hello Jenkins!");  // Instead of System.out.println
+        // No output, nothing happens
     }
 }
