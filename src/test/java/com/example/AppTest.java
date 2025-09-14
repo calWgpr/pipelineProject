ad94bce9c8dcbe8cc01ae899b410bfed03a5406b
Tsiory Fahasoavana <166718289+calWgpr@users.noreply.github.com>
2025-09-14 16:16:01 +0300

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AppTest {
+class AppTest {
 
     @Test
     void dummyTest() {
