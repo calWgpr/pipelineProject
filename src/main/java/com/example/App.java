b2a55995e2c88bf1b287949cfea7bf70ee99fb4c
Tsiory Fahasoavana <166718289+calWgpr@users.noreply.github.com>
2025-09-14 16:47:32 +0300

    last main app update

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,12 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        new App().run();
-    }
-
-    public String run() {
-        String msg = "App started";
-        System.out.println(msg);
-        return msg;
+        System.out.println("App started");
     }
 }
+
