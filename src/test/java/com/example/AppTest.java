0109552c4e7939d6e0b00a8497871678c343448b
Tsiory Fahasoavana <166718289+calWgpr@users.noreply.github.com>
2025-09-14 16:49:56 +0300

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,14 +1,9 @@
 package com.example;
-
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
-
     @Test
-    void testRun() {
-        App app = new App();
-        String result = app.run();
-        assertEquals("App started", result);
+    void shouldRunMain() {
+        App.main(new String[]{});
     }
 }
